97d40f685766188f4ef777b208d1cdd1d3c6aef3
Harsh Raj <harshxroy@gmail.com>
2025-08-27 16:56:25 +0530

    db connected & env.js completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,15 @@
-// Bring in the express library (it helps us create a web server)
 import express from 'express'
-
-// Bring in our settings (like port number, database link, etc.)
 import {ENV} from './config/env.js'
+import {connectDB} from './config/db.js'
 
 // Make a new express app (our web server)
 const app = express();
 
-// When someone goes to "http://localhost:PORT/"
-// send back "Hello World! 123" as the response
 app.get("/" , (req, res) => {
     res.send("Hello World! 123");
 });
 
-// Just print the database link on the console (for checking)
-console.log("mongo uri :" ,ENV.MONGO_URI);
-
-// Start the server on the given port number
-// and show a message when it has started
-app.listen(ENV.PORT , () => console.log("Server started on port : ", ENV.PORT));
+app.listen(ENV.PORT , () => {
+    console.log("Server started on port : ", ENV.PORT);
+    connectDB();
+});
